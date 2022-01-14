b29da45cf4b8039144e179842022da6dc3ae6445
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-01-14 14:42:17 -0600

    added contacts class - setup

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,57 +1,11 @@
-import java.util.Scanner;
+
 
 public class HelloWorld {
-
-
-    public static void runContactsApp(){
-        boolean run = true;
-        while(run){
-            System.out.println("1. View Contacts\n");
-            System.out.println("2, Add a new Contact.\n");
-            System.out.println("3. Search a contact by name.\n");
-            System.out.println("4. Delete an existing contact.\n");
-            System.out.println("5. Exit\n");
-            System.out.println("Enter an option: (1, 2, 3, 4, or 5\n");
-            Scanner scn = new Scanner(System.in);
-            int userinput = scn.nextInt();
-            switch (userinput){
-                case 1:
-                    // view contact/function
-                    System.out.println("Returned to Menu.\n");
-                    break;
-                case 2:
-                    // add contact method/function
-                    System.out.println("Returned to Menu.\n");
-                    break;
-                case 3:
-                    // add search contact by name method/function
-                    System.out.println("Returned to Menu.\n");
-                    break;
-                case 4:
-                    // delete contact (by name)
-                    System.out.println("Returned to Menu.\n");
-                    break;
-                case 5:
-                    // exit function/app
-                    System.out.println("Exiting application. ");
-                    run = false;
-                    break;
-            }
-        }
-    }
-
-
 
 
     public static void main(String[] args) {
 
 
-
-
-
-
-
-        runContactsApp();
 
 
     }
